b75d4ea0ab0d7f9c040dd127d902dc26cc6986f0
luochaojing <909048577@qq.com>
2020-02-02 23:37:41 +0800

    能按照教程将窗口跑起来

--- OpenGLES3/main.cpp
+++ OpenGLES3/main.cpp
@@ -7,47 +7,95 @@
 //
 
 #include <iostream>
+
+// GLEW
+#define GLEW_STATIC
 #include <GL/glew.h>
+
+// GLFW
 #include <GLFW/glfw3.h>
- 
-void Render(void)
+
+using namespace std;
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+// The MAIN function, from here we start the application and run the game loop
+int main()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_TRIANGLES);
-    {
-        glColor3f(1.0,0.0,0.0);
-        glVertex2f(0, .5);
-        glColor3f(0.0,1.0,0.0);
-        glVertex2f(-.5,-.5);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex2f(.5, -.5);
-    }
-    glEnd();
-}
- 
-int main(int argc, const char * argv[]) {
-    GLFWwindow* win;
-    if(!glfwInit()){
+    
+    std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+    // Init GLFW
+    if (!glfwInit()) {
+        std::cout << "glfwInit fialed" << endl;
         return -1;
     }
-    win = glfwCreateWindow(640, 480, "OpenGL Base Project", NULL, NULL);
-    if(!win)
+    // Set all the required options for GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+#if __APPLE__
+    // 在mac上做了兼容:https://github.com/ocornut/imgui/pull/229/commits/6ee65fc5afb6a0f14c88ec3d693d5fc8a2fcfb52
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
     {
+        const char *error;
+        glfwGetError(&error);
+        std::cout << "Failed to create GLFW window:" << error << std::endl;
         glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-    if(!glewInit())
-    {
         return -1;
     }
-    glfwMakeContextCurrent(win);
-    while(!glfwWindowShouldClose(win)){
-        Render();
-        glfwSwapBuffers(win);
-        glfwPollEvents();
+    glfwMakeContextCurrent(window);
+    // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
+
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    glewExperimental = GL_TRUE;
+    // Initialize GLEW to setup the OpenGL Function pointers
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
     }
+
+    // Define the viewport dimensions
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+
+        // Render
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
-    exit(EXIT_SUCCESS);
     return 0;
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    std::cout << key << std::endl;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
