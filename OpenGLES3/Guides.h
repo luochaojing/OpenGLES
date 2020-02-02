c09235098d13ac4070b245c89493aed425edb33e
luochaojing <909048577@qq.com>
2020-02-02 22:38:10 +0800

    成功创建窗口

---
+++ OpenGLES3/Guides.h
@@ -0,0 +1,24 @@
+//
+//  Guides.h
+//  OpenGLES3
+//
+//  Created by luochaojing on 2020/2/2.
+//  Copyright © 2020 lemon. All rights reserved.
+//
+
+#ifndef Guides_h
+#define Guides_h
+
+/*
+ ### 安装教程:
+ 1. 安装窗口
+ https://blog.csdn.net/SDUchen1998/article/details/89929801
+ 
+ 2. 遇到运行时动态库签名问题
+    1. 创建mac devloper: https://help.apple.com/developer-account/#/devbfa00fef7
+    2. 对动态库前面: https://blog.csdn.net/ReadyShowShow/article/details/102547633
+ 
+ 
+ */
+
+#endif /* Guides_h */
